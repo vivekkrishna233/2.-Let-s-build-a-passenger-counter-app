89d9bb157d3ba98953cbf36ac370996c6aeeae4a
vivek krishna <vivekkrishna233@gmail.com>
2022-03-10 18:58:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let myAge=35
+let humanDogRatio = 7
+let mydogAge= myAge * humanDogRatio
+console.log(mydogAge)
+
+let count = 50 
+count = count+100
+count=count-25
+count=count+70
+console.log(count)
